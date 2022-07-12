94dadc910486fb2033da74a6c6ff5b7e5398966e
Chris <chriseckert.ce@gmail.com>
2022-07-12 21:32:51 +0200

    fix(InputInfo): fill container with button

--- src/tailwind/ui/InputInfo/InputInfo.tsx
+++ src/tailwind/ui/InputInfo/InputInfo.tsx
@@ -6,32 +6,30 @@ export interface InputInfoProps {
   text: string;
 }
 
-export const InputInfo: FC<InputInfoProps> = (props) => {
+export const InputInfo = (props: InputInfoProps) => {
   const { text } = props;
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
   return (
-    <div className="relative h-full w-full flex justify-center items-center">
-      <ClickAwayListener onClickAway={() => setIsVisible(false)}>
-        <div className="flex justify-center items-center">
-          <button
-            type="button"
-            aria-label="info"
-            className="cursor-pointer border-none"
-            onClick={() => setIsVisible(!isVisible)}
+    <ClickAwayListener onClickAway={() => setIsVisible(false)}>
+      <div className="relative h-full w-full flex justify-center items-center">
+        <button
+          type="button"
+          aria-label="info"
+          className="cursor-pointer border-none h-full w-full flex justify-center items-center"
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          <AlertCircle />
+        </button>
+        {isVisible && (
+          <div
+            className="bg-white p-4 typo-body-xs absolute top-[100%] right-0 w-[250px] block drop-shadow-2xl rounded z-10"
+            role="alert"
           >
-            <AlertCircle />
-          </button>
-          {isVisible && (
-            <div
-              className="bg-white p-4 typo-body-xs absolute top-[100%] right-0 w-[250px] block drop-shadow-2xl rounded z-10"
-              role="alert"
-            >
-              <b>Hvorfor?</b> {text}
-            </div>
-          )}
-        </div>
-      </ClickAwayListener>
-    </div>
+            <b>Hvorfor?</b> {text}
+          </div>
+        )}
+      </div>
+    </ClickAwayListener>
   );
 };
